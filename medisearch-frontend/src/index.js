e285c66d7e29e3ed8293c4ff11d850d84ec362a2
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-07-03 19:19:20 +0530

    updated the UI with mantis dashboard

--- medisearch-frontend/src/index.js
+++ medisearch-frontend/src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+// third-party
+import { Provider as ReduxProvider } from 'react-redux';
+
+// apex-chart
+import 'assets/third-party/apex-chart.css';
+
+// project import
 import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
+import { store } from 'store';
+import reportWebVitals from './reportWebVitals';
+
+// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <StrictMode>
+        <ReduxProvider store={store}>
+            <BrowserRouter basename="/free">
+                <App />
+            </BrowserRouter>
+        </ReduxProvider>
+    </StrictMode>,
+    document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
