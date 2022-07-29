5b17acb3676aee12fc894a3387d42db8020d427f
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-07-29 22:18:02 +0530

    amplify configuration files added

---
+++ medisearch-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "medisearchdev9b8e48699b8e4869": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
